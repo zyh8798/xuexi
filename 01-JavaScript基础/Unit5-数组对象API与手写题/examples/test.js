0060564405fda9963120fac261d4c9c1994846f5
zyyyyyyyyyh <17673298578@163.com>
2026-03-04 18:02:29 +0800

    feat(unit5): 添加防抖节流和深拷贝示例代码

--- 01-JavaScript基础/Unit5-数组对象API与手写题/examples/test.js
+++ 01-JavaScript基础/Unit5-数组对象API与手写题/examples/test.js
@@ -1,10 +1,21 @@
-let obj = {}
-let arr = []
-let date = new Date()
-let reg = new RegExp()
-let n = null
-console.log(Object.prototype.toString.call(obj))
-console.log(Object.prototype.toString.call(arr))
-console.log(Object.prototype.toString.call(date))
-console.log(Object.prototype.toString.call(reg))
-console.log(Object.prototype.toString.call(n))
+// let obj = {}
+// let arr = []
+// let date = new Date()
+// let reg = new RegExp()
+// let n = null
+// console.log(Object.prototype.toString.call(obj))
+// console.log(Object.prototype.toString.call(arr))
+// console.log(Object.prototype.toString.call(date))
+// console.log(Object.prototype.toString.call(reg))
+// console.log(Object.prototype.toString.call(n))
+// 防抖
+function debounce(fn, delay) {
+    // 每次调用都创建新的setTimeout 用dealy做倒计时,倒计时完了执行fn
+    let timer
+    return function (...args) {
+        if (timer) clearTimeout(timer)
+        timer = setTimeout(() => {
+            fn.apply(this, args)
+        }, delay)
+    }
+}
